fe48e30bc718a9e2b89d2b6ae18fe2b744c840b5
Krukov Anton <meowskyj@yandex.ru>
2024-12-07 12:52:31 +0300

    fix send data

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -86,7 +86,9 @@ let dataGarden = null;
 
 axios.get(`/api/users/${chatId}`)
   .then(function (response) {
-    dataGarden = response.userData;
+    dataGarden = response;
+    
+    console.log('response', response);
     console.log('data garden', dataGarden);
     
   })
