b0805fe1f2b7a6dffb70f04761232ecf5c233e88
Krukov Anton <meowskyj@yandex.ru>
2024-12-07 20:31:49 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
     data.forEach((element, index) => {
       const dateNow = new Date();
       console.log('get time', dateNow.getTime());
-      console.log('element date', element.date.getTime());
+      console.log('element date', element.date);
       
       if (element.date && (dateNow.getTime() - element.date > element.riseTime)) {
         dispatch(setPlant({ index: index, plant: element.namePlant }));
