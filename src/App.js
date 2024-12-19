728031a71facb5ed3cb2e2427d98fa6ed274fb7e
Meowsky <meowskyj@yandex.ru>
2024-12-19 22:42:45 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
   }, [socket, loading, chatId]);
  // eslint-disable-next-line
   const debouncedUpdateData = useCallback(
-    debounce(updateData, 500),
+    debounce(updateData, 100),
     [updateData]
   );
 
