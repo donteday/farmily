6175f02041859e8119e3bde16dcccb5e0f8d3208
Meowsky <meowskyj@yandex.ru>
2024-12-21 22:28:16 +0300

    fix garden

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Snowfall from './components/Snowfall/Snowfall';
 import FriendGarden from './components/FriendGarden/FriendGarden';
 // import { fetchUserData } from './redux/store/store';
 
-var WebApp = window;
+var WebApp = window.Telegram.WebApp;
 
 function App() {
   
